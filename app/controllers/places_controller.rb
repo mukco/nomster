95ba1b6ee419688b287c0fbbdc714675e04427e9
Devoun <THREExPENNYco@gmail.com>
2019-08-05 21:10:15 +0000

    validation styling

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   end
 
   def create 
-    current_user.places.create(place_params) 
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid? 
+       redirect_to root_path
+    else 
+      render :new, status: :unprocessable_entity
+    end 
   end
 
   def new
@@ -39,7 +43,11 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
         return render plain: 'Not Allowed', status: :forbidden 
     end
     @place.update_attributes(place_params)
-    redirect_to place_path(params[:id])
+    if @place.valid? 
+      redirect_to root_path
+    else 
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
