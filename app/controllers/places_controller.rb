6b3ad12457497795e6def8b254f87c9051893679
Devoun <THREExPENNYco@gmail.com>
2019-08-06 04:33:00 +0000

    adding form for comments

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,52 +18,52 @@ class PlacesController < ApplicationController
   end 
 end
 
-def new
-  @place = Place.new 
-end 
+  def new
+    @place = Place.new 
+  end 
 
-def new_user 
-  @user = User.new 
-end 
+  def new_user 
+    @user = User.new 
+  end 
 
-def show
-  @place = Place.find(params[:id]) 
-end
-
-def edit 
-  @place = Place.find(params[:id])
-  if @place.user != current_user
-    return render plain: 'Not Allowed', status: :forbidden
+  def show
+    @place = Place.find(params[:id]) 
+    @comment = Comment.new
   end
-end
 
-def update
-  @place = Place.find(params[:id])
-  if @place.user != current_user 
-    return render plain: 'Not Allowed', status: :forbidden 
+  def edit 
+    @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
   end
-  @place.update_attributes(place_params)
-  if @place.valid? 
+
+  def update
+    @place = Place.find(params[:id])
+    if @place.user != current_user 
+      return render plain: 'Not Allowed', status: :forbidden 
+    end
+    @place.update_attributes(place_params)
+    if @place.valid? 
+      redirect_to root_path
+    else 
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    if @place.user != current_user 
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+    @place.destroy 
     redirect_to root_path
-  else 
-    render :edit, status: :unprocessable_entity
+  end 
+
+  private 
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
   end
-end
-
-def destroy
-  @place = Place.find(params[:id])
-  if @place.user != current_user 
-    return render plain: 'Not Allowed', status: :forbidden
-  end
-  @place.destroy 
-  redirect_to root_path
-end 
-
-private 
-
-def place_params
-  params.require(:place).permit(:name, :description, :address)
-end
-
 
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user 
-  has_mant :comments
+  has_many :comments
   geocoded_by :address 
   after_validation :geocode 
   validates :name, presence: true, length: {minimum: 3}
