e0bdee095ebaeca753cdc3658f36346a35b6672f
Devoun <THREExPENNYco@gmail.com>
2019-08-06 04:00:04 +0000

    adding database for comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user 
+  has_mant :comments
   geocoded_by :address 
   after_validation :geocode 
   validates :name, presence: true, length: {minimum: 3}
